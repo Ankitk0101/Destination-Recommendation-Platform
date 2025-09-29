27acac91fa7f51418f51af3c3552bb43b9368867
Ankit Kumar <ankit933480@gmail.com>
2025-09-29 14:24:44 +0530

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL ='https://destination-recommendation-platform-2.onrender.com';
+const API_BASE_URL ='https://destination-recommendation-platform-1.onrender.com/api';
 
 class ApiService {
   constructor() {
